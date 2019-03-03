ec83bdc7c152eee4da40c8df58c7d8fb081ff5c9
quanletian <>
2019-03-03 21:58:45 +0800

    提交测试类添加方法

--- quanle_web/src/main/java/com/quanle/test.java
+++ quanle_web/src/main/java/com/quanle/test.java
@@ -1,4 +1,14 @@
 package com.quanle;
 
+/**
+ * @author quanle
+ * 测试类
+ */
 public class test {
+    public static void main(String [] args){
+        asd();
+    }
+ public static void asd(){
+     System.out.println("test.asd");
+ }
 }
